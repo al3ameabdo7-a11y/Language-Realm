8cabd831461f5dcfc428a595f094f1243c7e74b5
al3ameabdo7-a11y <al3ameabdo7@gmail.com>
2026-01-12 23:15:57 +0000

    Ss

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+  <Router hook={useHashLocation}>
     <App />
-  </HashRouter>
+  </Router>
 );
